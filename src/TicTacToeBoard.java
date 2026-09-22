3c35e62a62c4692376f5ac434fa5f3eff56c0aa3
128jazi-sys <128jaziz@frhsd.com>
2026-09-22 13:31:25 -0400

    completed instance variables

---
+++ src/TicTacToeBoard.java
@@ -0,0 +1,13 @@
+public class TicTacToeBoard {
+    private int[][] board;
+    public int EMPTY = 0;
+    public int PLAYER_X = 1;
+    public int PLAYER_O = 2;
+
+    public TicTacToeBoard() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
