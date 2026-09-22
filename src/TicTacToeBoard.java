9087d81caea1c45f10744761ad01c949752ef948
128jazi-sys <128jaziz@frhsd.com>
2026-09-22 13:42:50 -0400

    wrote out methods with proper syntax

--- src/TicTacToeBoard.java
+++ src/TicTacToeBoard.java
@@ -7,6 +7,57 @@ public class TicTacToeBoard {
     public TicTacToeBoard() {
 
     }
+
+    public int getCell(int row, int col) {
+        return 1;
+    }
+
+    public boolean placeMove(int row, int col, int player) {
+        return true;
+    }
+
+    public boolean isValidMove(int row, int col) {
+        return true;
+    }
+
+    public int checkRowWin(int row) {
+        return 1;
+    }
+
+    public int checkColWin(int col) {
+        return 1;
+    }
+
+    public int checkMainDiagonalWin() {
+        return 1;
+    }
+
+    public int checkAntiDiagonalWin() {
+        return 1;
+    }
+
+    public int checkWinner() {
+        return 1;
+    }
+
+    public boolean isFull() {
+        return true;
+    }
+
+    public void displayBoard () {
+        System.out.print("display");
+    }
+
+    public void resetBoard () {
+        System.out.print("reset");
+    }
+
+
+@Nested
+class TTTTester {
+
+}
+
     public static void main(String[] args) {
 
     }
